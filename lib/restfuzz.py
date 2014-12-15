313b51ad643bbb6dea4a04c9fe6d3a03bce7df1d
Lovro Predovan <lovro.predovan@gmail.com>
2014-12-15 23:48:39 +0100

    added saving results in json file in subdirecotry

--- lib/restfuzz.py
+++ lib/restfuzz.py
@@ -1,6 +1,12 @@
 __author__ = 'lovro'
 
+import httplib2
+import json
+import datetime
+import time
+
 from pywebfuzz import utils
+
 
 class RestFuzzer:
     def __init__(self):
@@ -10,8 +16,24 @@ class RestFuzzer:
         print "fuzzing rest"
 
 
-        #https://code.google.com/p/pywebfuzz/wiki/UsageExamples
-        result = utils.make_request('http://localhost:8000/api/v1/locations')
+        # https://code.google.com/p/pywebfuzz/wiki/UsageExamples
+        # result = utils.make_request('http://localhost:8000/api/v1/locations')
 
-        #print( headers)
+        result = []
+
+        for x in range(0, 5):
+            h = httplib2.Http(".cache")
+            resp, content = h.request('http://localhost:8000/api/v1/locations', "GET")
+
+            item = {'id': x, 'response': resp, 'content': content, 'length': len(content)}
+            result.append(item)
+
+        #resopnse
+        #print resp
+
+        #response content
         print result
+
+        ts = time.time()
+        current_time = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')
+        with open('results/rest_fuzz_result_'+current_time+'.json', 'w') as outfile: json.dump(result, outfile)
