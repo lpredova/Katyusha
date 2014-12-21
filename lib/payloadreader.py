bd05b8c921340147bfa5cbcfe523aa40b8fef87c
Milan Pavlović <bluraymimi@gmail.com>
2014-12-21 16:59:28 +0100

    created basic class for reading fuzz strings

---
+++ lib/payloadreader.py
@@ -0,0 +1,31 @@
+__author__ = 'Milan'
+
+class PayloadReader:
+
+    _filePath = 'data/MySQL.fuzz.txt'
+
+    def __init__(self):
+        pass
+
+    def setFilePath(self, path):
+        self._filePath = path
+
+    def getFilePath(self):
+        return self._filePath
+
+    def getFuzzStrings(self):
+        fuzzFile = open(self._filePath, "r")
+        fuzzVectors = []
+
+        for line in fuzzFile:
+            fuzzVectors.append(line.strip())
+
+        fuzzFile.close()
+        return fuzzVectors
+
+
+reader = PayloadReader()
+
+strings = reader.getFuzzStrings()
+
+print strings
